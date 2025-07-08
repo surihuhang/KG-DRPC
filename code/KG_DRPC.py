2c6d4d35c590c3e3c77e2463e367ed56f596bf9a
胡航 <103475889+surihuhang@users.noreply.github.com>
2025-07-08 21:06:13 +0800

    Update KG_DRPC.py

--- code/KG_DRPC.py
+++ code/KG_DRPC.py
@@ -1,7 +1,6 @@
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
 import numpy as np
 import pandas as pd
 from ampligraph.latent_features import ComplEx,TransE,DistMult
